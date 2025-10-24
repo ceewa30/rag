7c5b8dc1d55e6a1ef59b17aa4903e0556e875777
ceewa30 <ceewa30@gmail.com>
2025-10-24 16:49:44 -0400

    creating a data_loader

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from rag!")
-
+from src.data_loader import load_all_documents
 
 if __name__ == "__main__":
-    main()
+    data_directory = "./data"  # Adjust the path as needed
+    docs = load_all_documents(data_directory)
+    print(f"Total documents loaded: {len(docs)}")
+    for i, doc in enumerate(docs):
+        print(f"Document {i+1} : {doc.metadata}")
